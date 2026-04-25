9d586aa1df340af3c5401d7819e98fdf5af9a69b
sardororziqulov5-ui <sardororziqulov5@gmail.com>
2026-04-25 11:24:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 
 BOT_TOKEN = os.getenv("8098097569:AAGP9XoDKtB_JWp14G8IrmBp96kRdYIDf08")  # TOKENNI ENV GA QO‘Y
-bot = telebot.TeleBot(8098097569:AAGP9XoDKtB_JWp14G8IrmBp96kRdYIDf08)
+bot = telebot.TeleBot(BOT_TOKEN)
 
 L = instaloader.Instaloader(download_pictures=True, download_videos=True)
 
